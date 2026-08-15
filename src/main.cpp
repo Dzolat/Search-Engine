ba9f8691d744d317ef82740011c382b7585132cc
RealDzolat <dzolbusines@gmail.com>
2026-08-15 22:47:08 +0200

    Added file check to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
-int main()
+#include "../include/document.hpp"
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!";
+    using namespace std::filesystem;
+    std::vector<Document> doc_vector;
+
+    if (argc != 2)
+    {
+        std::cout << "Usage: ./search.exe [path]";
+        return 1;
+    }
+
+    path directory_path = argv[1];
+
+    if (!exists(directory_path))
+    {
+        std::cout << "The directory doesnt exist.";
+        return 2;
+    }
 }
