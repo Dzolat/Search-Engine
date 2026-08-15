1b1491cdd81098a0814854625b69bb89d0e1e8ab
RealDzolat <dzolbusines@gmail.com>
2026-08-16 01:52:52 +0200

    Added search

---
+++ include/Search.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <unordered_map>
+
+#include "Tokenizer.hpp"
+#include "document.hpp"
+
+void start_search(std::vector<Document>& doc_vector, std::unordered_map<std::string, std::unordered_map<int, int>> &index);
---
+++ src/Search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+#include "../include/Tokenizer.hpp"
+#include "../include/document.hpp"
+
+void start_search(std::vector<Document>& doc_vector, std::unordered_map<std::string, std::unordered_map<int, int>> &index)
+{
+    std::string phrase;
+
+    std::cout << "\nsearch> ";
+    std::cin >> phrase;
+
+    std::unordered_map<int, int> scores;
+    for (const auto &query_word : tokenize(phrase))
+    {
+
+        auto it = index.find(phrase);
+
+        if (it == index.end())
+        {
+            continue;
+        }
+
+        for (const auto &[document_id, frequency] : it->second)
+        {
+            scores[document_id] += frequency;
+        }
+    }
+
+    std::vector<std::pair<int, double>> results(scores.begin(), scores.end());
+    std::sort(results.begin(), results.end(), [](const auto &a, const auto &b)
+              { return a.second > b.second; });
+
+    int counter = 1;
+    for (auto result : results)
+    {
+        
+        std::cout << "\n" << counter << ". " << doc_vector[result.first].path << "\n   score: " << result.second << "\n";
+        counter++;
+        if (counter == 3)
+            break;
+    }
+}
